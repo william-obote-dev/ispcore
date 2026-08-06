a690f509dc346325b303fd4cc46f054906ad6dff
William Obote <68078982+william-obote-dev@users.noreply.github.com>
2026-08-06 06:00:49 +0000

    Added paystack

---
+++ config/paystack.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'secret_key' => env('PAYSTACK_SECRET_KEY'),
+    'public_key' => env('PAYSTACK_PUBLIC_KEY'),
+    'base_url' => env('PAYSTACK_BASE_URL', 'https://api.paystack.co'),
+];
