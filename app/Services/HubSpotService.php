0313b986b6425352d9e6bc679c85e70fa8f5a8fb
William Obote <68078982+william-obote-dev@users.noreply.github.com>
2026-08-10 16:30:19 +0000

    Add HubSpot contact sync

---
+++ app/Services/HubSpotService.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Http;
+
+class HubSpotService
+{
+    private function client()
+    {
+        return Http::withToken(config('hubspot.access_token'))
+            ->baseUrl(config('hubspot.base_url'));
+    }
+
+    public function findOrCreateContact(string $name, string $email, string $phone): string
+    {
+        $existing = $this->client()->post('/crm/v3/objects/contacts/search', [
+            'filterGroups' => [[
+                'filters' => [[
+                    'propertyName' => 'email',
+                    'operator' => 'EQ',
+                    'value' => $email,
+                ]],
+            ]],
+        ]);
+
+        $results = $existing->json('results');
+
+        if (! empty($results)) {
+            return $results[0]['id'];
+        }
+
+        [$firstName, $lastName] = array_pad(explode(' ', $name, 2), 2, '');
+
+        $created = $this->client()->post('/crm/v3/objects/contacts', [
+            'properties' => [
+                'email' => $email,
+                'firstname' => $firstName,
+                'lastname' => $lastName,
+                'phone' => $phone,
+            ],
+        ]);
+
+        if (! $created->successful()) {
+            throw new \RuntimeException('HubSpot contact creation failed: ' . $created->body());
+        }
+
+        return $created->json('id');
+    }
+}
---
+++ config/hubspot.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'access_token' => env('HUBSPOT_ACCESS_TOKEN'),
+    'base_url' => env('HUBSPOT_BASE_URL', 'https://api.hubapi.com'),
+];
