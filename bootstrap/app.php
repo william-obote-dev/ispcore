53224bfecd33e11c0515720a16188607ed485e47
William Obote <68078982+william-obote-dev@users.noreply.github.com>
2026-08-16 15:48:39 +0000

    Fix: disable login redirect for API-only app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,8 +13,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
-    })
+    $middleware->redirectGuestsTo(fn () => null);
+     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
             fn (Request $request) => $request->is('api/*'),
